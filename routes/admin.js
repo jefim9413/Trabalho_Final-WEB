c17e85145d3832b849d709af971ab9ccfb657a05
jefim9413 <jefferson.jsm3444@gmail.com>
2020-09-27 01:16:19 -0300

    .

--- routes/admin.js
+++ routes/admin.js
@@ -9,8 +9,13 @@ router.get('/', (req, res) => {
     res.render('admin/index');
 });
 
-router.get("/categoria",(req,res)=>{
-    res.render("admin/categorias")
+router.get('/categoria', (req, res) => {
+    Categoria.find().sort({Data: 'desc'}).then((categoria) => {
+        res.render('./admin/categorias', {categorias: categoria.map(Categoria => Categoria.toJSON())})
+    }).catch((error) => {
+        console.log('houve um erro ao listar as categorias ' + erro)
+        res.redirect('/admin')
+    })
 })
 router.post("/categorias/nova",(req,res)=>{
 
@@ -38,11 +43,40 @@ router.post("/categorias/nova",(req,res)=>{
             res.redirect("/admin/categoria")
         }).catch((err) =>{
             req.flash("error_msg","Houve um Erro ao Salvar, Tente Novamente!")
-            res.redirect("/admin")
+            res.redirect("/admin/categoria")
         })
     }
 
 })
+
+router.get('/categoria/edit/:id',(req,res) =>{
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria)=>{
+        res.render('admin/editcategoria', {categoria:categoria})
+    }).catch((err) =>{
+         req.flash("error_msg","Está Categoria não existe")
+         res.redirect("/admin/categoria")
+    })
+    
+})
+
+router.post('/categoria/edit',(req,res)=>{
+    Categoria.findOne({_id:req.body.id}).then((categoria) =>{
+        categoria.Nome = req.body.name
+        categoria.Slug = req.body.slug
+
+        categoria.save().then(()=>{
+            req.flash("success_msg", "Categoria Editada com Sucesso")
+            res.redirect("/admin/categoria")
+        }).catch((err) =>{
+            req.flash("error_msg","Houve um Erro ao salvar a Edição da Categoria !")
+            res.redirect("/admin/categoria")
+        })
+    }).catch((err) =>{
+        req.flash("error_msg","Houve um Erro ao Editar a Categoria!")
+        res.redirect("/admin/categoria")
+    })
+})
+
 router.get('/categoria/add',(req,res) =>{
     res.render("admin/addcategoria")
 })
