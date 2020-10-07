44b1515f02a83df612fb4a47c2650a35266d5232
jefim9413 <jefferson.jsm3444@gmail.com>
2020-10-06 21:49:52 -0300

    ok

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ const db = require("./config/db")
         Postagens.find().lean().populate("Categoria").populate("Raridade").sort({Data: "desc"}).then((postagens)=>{
             res.render("index",{postagens:postagens})
         }).catch((err)=>{
+            console.log(err)
             req.flash("error_msg","Houve um erro interno")
             res.redirect("/404")
         })
