ac3f6c310c12bd20a6eab6f555e0ff3867627034
jefim9413 <jefferson.jsm3444@gmail.com>
2020-10-12 12:44:36 -0300

    ok

--- routes/admin.js
+++ routes/admin.js
@@ -145,9 +145,9 @@ router.post("/categorias/nova",eAdmin,(req,res)=>{
 
 })
 
-router.get('/categoria/edit/:id',eAdmin,(req,res)=>{
+router.get("/categoria/edit/:id",eAdmin,(req,res)=>{
     Categoria.findOne({_id:req.params.id}).lean().then((categoria)=>{
-        res.render('admin/editcategoria', {categoria:categoria})
+        res.render('admin/editcategoria',{categoria:categoria})
     }).catch((err) =>{
          req.flash("error_msg","Está Categoria não existe")
          res.redirect("/admin/categoria")
